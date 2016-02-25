6ca62ff5904aaf3562937f4b79f3e6fad1a6771a
hoshin <martin.bahier@gmail.com>
2016-02-25 16:19:33 +0100

    fixed the sample configuration file that was not correctly updated to reflect the new configuration arrangements

--- ludwig-conf-sample.js
+++ ludwig-conf-sample.js
@@ -1,19 +1,23 @@
 module.exports = {
-    url: 'https://github.com/user/repo/new/master',
+    repoUrl: 'https://github.com/user/repo/new/master',
     template: 'some+basic+template+you+want',
     prefix: 'prefix for suggestions',
-    commitReferenceToBranchFrom:'<commit sha1 reference from master to branch from>',
-    github:{
-        callback_url:'http://authentication.callback.url/for/github/login',
-        apiEndpoints:{
-            createRef:'https://api.github.com/repos/user/repo/git/refs',
-            createContent:'https://api.github.com/repos/user/repo/contents/',
-            createPullRequest:'https://api.github.com/repos/user/repo/pulls'
-        }
-    },
+	expectedTemplate:'{}',
+	ludwigCreateSuggestionURL: 'http://localhost:3000/createSuggestion',
     web: {
         accepted_tests_path: '/tree/master/tests',
         add_path: '/new/master',
         suggested_tests_path: '/pulls?utf8=✓&q=is%3Apr+is%3Aopen'
+    },
+
+	commitReferenceToBranchFrom:'<commit sha1 reference from master to branch from>',
+    github:{
+        callback_url:'http://authentication.callback.url/for/github/login',
+      
+	  	apiEndpoints:{
+            createRef:'https://api.github.com/repos/user/repo/git/refs',
+            createContent:'https://api.github.com/repos/user/repo/contents/',
+            createPullRequest:'https://api.github.com/repos/user/repo/pulls'
+        }
     }
 };
