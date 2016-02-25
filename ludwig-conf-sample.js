a08f066849046da87672e06e759203c9a1950050
hoshin <martin.bahier@gmail.com>
2016-02-25 10:43:45 +0100

    updated linting rules to check root js files + linted root js files

--- ludwig-conf-sample.js
+++ ludwig-conf-sample.js
@@ -1,20 +1,20 @@
 module.exports = {
-    url: 'https://github.com/user/repo/new/master',
-    template: 'some+basic+template+you+want',
-    prefix: 'prefix for suggestions',
-    testFeaturesEnabled:false,
-    cors:{
-        "Access-Control-Allow-Origin":'http://you.should.probably.edit.this/*'
-    },
-    commitReferenceToBranchFrom:'<commit sha1 reference from master to branch from>',
-    github:{
-        client_id:'<app client ID>',
-        client_secret:'<app client secret>',
-        callback_url:'http://authentication.callback.url/for/github/login',
-        apiEndpoints:{
-            createRef:'https://api.github.com/repos/user/repo/git/refs',
-            createContent:'https://api.github.com/repos/user/repo/contents/',
-            createPullRequest:'https://api.github.com/repos/user/repo/pulls'
-        }
-    }
-};
+	url: 'https://github.com/user/repo/new/master',
+	template: 'some+basic+template+you+want',
+	prefix: 'prefix for suggestions',
+	testFeaturesEnabled: false,
+	cors: {
+		'Access-Control-Allow-Origin': 'http://you.should.probably.edit.this/*'
+	},
+	commitReferenceToBranchFrom: '<commit sha1 reference from master to branch from>',
+	github: {
+		client_id: '<app client ID>',
+		client_secret: '<app client secret>',
+		callback_url: 'http://authentication.callback.url/for/github/login',
+		apiEndpoints: {
+			createRef: 'https://api.github.com/repos/user/repo/git/refs',
+			createContent: 'https://api.github.com/repos/user/repo/contents/',
+			createPullRequest: 'https://api.github.com/repos/user/repo/pulls'
+		}
+	}
+}
--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-'use strict';
-var express = require('express'), path = require('path');
-var session = require('express-session');
-var passport = require('passport');
-var ludwigConfig = require('./ludwig-conf');
+'use strict'
+var express = require('express'), path = require('path')
+var session = require('express-session')
+var passport = require('passport')
+var ludwigConfig = require('./ludwig-conf')
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-var app = express();
+var app = express()
 
-var env = app.get('env');
+var env = app.get('env')
 var config = {
-	port:env['NODE_PORT'] || 3000,
-	ip:env['NODE_IP'] || 'localhost'
-};
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '/dist')));
+	port: env['NODE_PORT'] || 3000,
+	ip: env['NODE_IP'] || 'localhost'
+}
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/dist')))
 app.use(session({
-    secret: 'we will need to be able to configure this'
-}));
+	secret: 'we will need to be able to configure this'
+}))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', ludwigConfig.cors['Access-Control-Allow-Origin']);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', ludwigConfig.cors['Access-Control-Allow-Origin'])
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	next()
+})
 
-app.use('/', require('./routers/main'));
+app.use('/', require('./routers/main'))
 
 app.listen(config.port, config.ip, function () {
-    console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
-    if(ludwigConfig.testFeaturesEnabled){
-        console.info('CAUTION : Test features are enabled. If you are trying to run a production instance, you should probably disable this by setting the testFeaturesEnabled value to false in ludwig-conf.js');
-    }
-});
+	console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'))
+	if (ludwigConfig.testFeaturesEnabled) {
+		console.info('CAUTION : Test features are enabled. If you are trying to run a production instance, you should probably disable this by setting the testFeaturesEnabled value to false in ludwig-conf.js')
+	}
+})
 
-exports = module.exports = app;
+exports = module.exports = app
