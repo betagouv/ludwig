ef1cef18cd3ed8fc6536abe877d86ee18fc880aa
hoshin <martin.bahier@gmail.com>
2016-03-02 17:49:49 +0100

    fixed webpack dependency issue (not properly included) also removed gulp references as it is not necessary right now

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
-
-gulp.task('default', function () {
-	return gulp.src('./js/ludwidget.js')
-		.pipe(webpack(require('./webpack.config.js')))
-		.pipe(gulp.dest('./'));
-});
