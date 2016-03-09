4e01b8ffecb55440671e841da2d832f3497f6b4b
hoshin <martin.bahier@gmail.com>
2016-03-09 14:53:26 +0100

    Added a "runner" program that runs the testResultsCollector and tries to parse an xunit report (given as an argument)

--- batch/runner.js
+++ batch/runner.js
@@ -1,3 +1,27 @@
-/**
- * Created by martin on 3/8/16.
- */
+import {TestResultsCollector} from './testResultsCollector';
+
+let configuration = {
+	mongo: {
+		uri: 'mongodb://localhost/ludwig',
+		options: {}
+	}
+};
+
+const collector = new TestResultsCollector(configuration);
+
+if (process.argv[2]) {
+	collector.saveFromXUnitData(process.argv[2], (err, data) => {
+		if (err) {
+			console.error('Something wrong happened');
+			console.error(err);
+			process.exit(1);
+		} else {
+			console.log('Data inserted!');
+			process.exit(0);
+		}
+	});
+} else {
+	console.log('You need to specify a file');
+	console.log('Usage : babel-node runner.js <xunit_report.xml>');
+	process.exit(1);
+}
