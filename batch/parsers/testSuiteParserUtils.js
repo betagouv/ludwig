6dce6b742bbb0aa604cb5d1d51cf5e672768e45c
hoshin <martin.bahier@gmail.com>
2016-05-02 10:22:26 +0200

    Remove unecessary property name escapes

--- batch/parsers/testSuiteParserUtils.js
+++ batch/parsers/testSuiteParserUtils.js
@@ -13,18 +13,18 @@ function parseIntOrZero(stringToParse) {
 }
 export function testSuiteSummary(raw) {
 	return {
-		'tests': parseIntOrZero(raw.tests),
-		'failures': parseIntOrZero(raw.failures),
-		'skipped': parseIntOrZero(raw.skipped),
-		'errors': parseIntOrZero(raw.errors)
+		tests: parseIntOrZero(raw.tests),
+		failures: parseIntOrZero(raw.failures),
+		skipped: parseIntOrZero(raw.skipped),
+		errors: parseIntOrZero(raw.errors)
 	};
 }
 
 export function getFailureDataForSingleTest(raw) {
 	return {
-		'type': raw.$.type,
-		'message': raw.$.message,
-		'raw': raw._
+		type: raw.$.type,
+		message: raw.$.message,
+		raw: raw._
 	};
 }
 
@@ -33,12 +33,14 @@ export function tests(raw) {
 		var current = (test.failure || [])[0] || {$: {}, _: ''};
 
 		return _({
-			'name': test.$.name,
-			'time': normalizeTime(test.$.time),
-			'classname': test.$.classname,
-			'failure': getFailureDataForSingleTest(current)
+			name: test.$.name,
+			time: normalizeTime(test.$.time),
+			classname: test.$.classname,
+			failure: getFailureDataForSingleTest(current)
 		}).tap(function (result) {
-			!test.failure && delete result.failure;
+			if (!test.failure) {
+				delete result.failure;
+			}
 		}).value();
 	}).value();
 }
@@ -60,12 +62,12 @@ export function gatherTestSuiteData(raw) {
 		return {};
 	}
 	var parsed = {
-		'name': testSuiteFromParsedData.$.name,
-		'time': normalizeTime(testSuiteFromParsedData.$.time),
-		'summary': testSuiteSummary(testSuiteFromParsedData.$),
-		'tests': tests(testSuiteFromParsedData.testcase),
-		'outputs': getOutputs(testSuiteFromParsedData),
-		'timestamp': testSuiteFromParsedData.$.timestamp ? Date.parse(testSuiteFromParsedData.$.timestamp) : new Date().getTime()
+		name: testSuiteFromParsedData.$.name,
+		time: normalizeTime(testSuiteFromParsedData.$.time),
+		summary: testSuiteSummary(testSuiteFromParsedData.$),
+		tests: tests(testSuiteFromParsedData.testcase),
+		outputs: getOutputs(testSuiteFromParsedData),
+		timestamp: testSuiteFromParsedData.$.timestamp ? Date.parse(testSuiteFromParsedData.$.timestamp) : new Date().getTime()
 	};
 	return {'suite': parsed};
 }
