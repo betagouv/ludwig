aad25200e95a94f1957e27793d0e850d2612942f
Thomas Guillet <guillet.thomas@gmail.com>
2018-04-07 18:40:48 +0200

    Update initial log message
    * URL allow quick access to the web page

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ var app = require('./app')()
 var port = 4000
 
 app.listen(port, () => {
-  console.log('Ludwig server is listening on port %d, in %s mode.', port, app.get('env'))
+  console.log(`Ludwig server is listening at http://localhost:${port}/, in ${app.get('env')} mode.`)
 })
