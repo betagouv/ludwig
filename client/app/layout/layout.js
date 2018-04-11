48584319cde55410809385d9d11d52bcf46e5e95
Thomas Guillet <guillet.thomas@gmail.com>
2018-04-11 10:45:31 +0200

    Structure client layout

--- client/app/layout/layout.js
+++ client/app/layout/layout.js
@@ -3,9 +3,9 @@
 angular.module('ludwigApp')
   .config(function ($stateProvider) {
     $stateProvider
-      .state('layout.main', {
+      .state('layout', {
         url: '',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'app/layout/layout.html',
+        abstract: true
       })
   })
