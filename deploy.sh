16c0df78d5b825c85a237f0c9f0a5dc4d76dc7b2
Thomas Guillet <guillet.thomas@gmail.com>
2018-11-28 18:31:35 +0100

    Ensure SSH keys are set appropriately on server

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,24 @@ set -ev
 # Get latest master
 git pull
 
+# Ensure SSH keys are appropriately set
+AUTHORIZED_KEYS_FILE=/home/cloud/.ssh/authorized_keys
+echo 'command="/home/cloud/ludwig/deploy.sh",no-pty,no-port-forwarding,no-agent-forwarding ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDoD3h4KJfjrGmRkYBd3/+gtaQTVautl/W8P258puydWx/ON5gZeyNuYBMzBaix9SfZk5qkY+U6y1PrnqKqjYQLJVU5IVcHkgaBGREQmOCQyfdBvpuXBeAVvmqEodeS32PtjNVC8F32CMwFZxJs0dCpMWyPkX2cLElZrqZdX6n+Ki2BbpHTwcEi2YRM6gmKxMStsYVxjTCpx90B2lnNuByoIb+saz5+g8ivF3XySxLQKJ72g8NC6mg1FZewsTCmvfpPL3gX3+v62CRjRMb68YgCBFAW7aKv/9XtNCr9jo+icpDcpD3js9/qvHKHE0yJEKSj1U2MsHdGP1qivRGf4iGB CircleCI' > $AUTHORIZED_KEYS_FILE
+curl --silent https://github.com/guillett.keys >> $AUTHORIZED_KEYS_FILE
 
-ls /home/cloud/.ssh
-cat /home/cloud/.ssh/authorized_keys
+# Update packages
+npm install
 
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3mAQSj+tW6j4flrRJCc6GKupoLhM4Wjb2PcwVDmU8/eY7homCu4o6mvN92NfnKojxJDuemi5jNvxTIOuH4eUnCQ5753FsiE8TiJQf+r3VfMP/Yu5pCRugztXE2B0TaFaM4J58LRPDEkQK4Rt7gA+1vrQSfkymzztgERZ7sqta4s+3SB+HdibQaXIP20cmalpEpBdxtLBwERx9Lqsarfzj6WjRjfiYObQHvxCnM6TxVeJz56XuRJhuDGKdR7LWW/C7Lx8I2gmGFt/D4UbOijhzRRN8dMxFXQIAeXIQVVyxv9GVEj4Kch2mbk8XrsdDURgcxRzUjOu4WzdupvbxVP/j thomas@Rutabaga' >> /home/cloud/.ssh/authorized_keys
+# Restart server
+sudo systemctl daemon-reload
+sudo systemctl restart ludwig
+
+git log -n 1
+
+# Show last modified time
+ls -ld .git
+
+
+# Show status
+sleep 1
+sudo systemctl status ludwig
