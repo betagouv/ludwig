ee8d62d2f0e866ac90a910a5b44aaf0f6965817b
Thomas Guillet <guillet.thomas@gmail.com>
2018-11-28 17:58:12 +0100

    Debug production access

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,6 @@ set -ev
 # Get latest master
 git pull
 
-# Update packages
-npm install
 
-# Restart server
-sudo systemctl daemon-reload
-sudo systemctl restart ludwig
-
-git log -n 1
-
-# Show last modified time
-ls -ld .git
-
-
-# Show status
-sleep 1
-sudo systemctl status ludwig
+ls /home/cloud/.ssh
+cat /home/cloud/.ssh/authorized_keys
