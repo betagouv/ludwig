48118c5a6e68c82a4d0488c72cf58fe1cb9382d8
Thomas Guillet <guillet.thomas@gmail.com>
2019-01-28 09:23:48 +0100

    Reword homepage content

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,24 +1,4 @@
 'use strict'
 
 angular.module('ludwigApp')
-  .controller('HomeCtrl', function ($scope, LoginService, RepositoryService) {
-    $scope.auth = {}
-    LoginService.get()
-      .then(function (identities) {
-        $scope.auth.identities = identities
-
-        RepositoryService.getCandidates()
-          .then(function (repositories) {
-            $scope.repositories = repositories
-          })
-      })
-
-    $scope.activateRepository = RepositoryService.activateRepository
-
-    $scope.logout = function () {
-      LoginService.delete()
-        .then(function (result) {
-          $scope.auth.logout = result
-        })
-    }
-  })
+  .controller('HomeCtrl', function () {})
